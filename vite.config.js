36073852ba8d5a19831fa7139a334e5ca0fd9505
alexis <alexis@LAPTOP-C1NLR7AV.localdomain>
2025-10-07 12:43:46 -0300

    fix: vite.config modificado para funicionar con CORS

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,13 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
 
 // https://vite.dev/config/
+
+// Es para que corra que funcione con el back utilizando CORS
 export default defineConfig({
   plugins: [react()],
+  server: {
+    proxy: {
+      '/api': 'http://localhost:3000', // ⬅️ backend
+    },
+  },
 })
